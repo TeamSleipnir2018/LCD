4b8cd4c2532077248bcf4acbdb4f669c8c875aa8
einara16 <einara16@e8f0e427-c348-4ead-811b-bab38cd7d69e>
2018-02-28 16:14:45 +0000

    Added Shift register support for LED bars
    git-svn-id: https://repository.cs.ru.is/svn/sleipnir/2018/LCD/trunk@70 e8f0e427-c348-4ead-811b-bab38cd7d69e

--- __vm/.LCD.vsarduino.h
+++ __vm/.LCD.vsarduino.h
@@ -5,13 +5,18 @@
 			all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
+<<<<<<< HEAD
 	Hardware: Teensy 3.2 / 3.1, Platform=teensy3, Package=teensy
+=======
+	Hardware: Arduino Pro or Pro Mini w/ ATmega328 (5V, 16 MHz), Platform=avr, Package=arduino
+>>>>>>> Added Shift register support for LED bars
 */
 
 #if defined(_VMICRO_INTELLISENSE)
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
+<<<<<<< HEAD
 #define __HARDWARE_MK20dx256__
 #define __HARDWARE_MK20DX256__
 #define __MK20DX256__
@@ -20,6 +25,14 @@
 #define F_CPU 96000000
 #define USB_SERIAL
 #define LAYOUT_US_ENGLISH
+=======
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
+#define F_CPU 16000000L
+#define ARDUINO 106011
+#define ARDUINO_AVR_PRO
+#define ARDUINO_ARCH_AVR
+>>>>>>> Added Shift register support for LED bars
 #define __cplusplus 201103L
 #define __arm__
 #define __ARM__
@@ -37,6 +50,7 @@ typedef void *__builtin_va_list;
 #define NEW_H
 
 
+<<<<<<< HEAD
 #include <arduino.h>
 #define abs(x) ((x)>0?(x):-(x))
 #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
@@ -56,6 +70,14 @@ typedef void *__builtin_va_list;
 #define __inline__
 #define __asm__ 
 #define __volatile__
+=======
+#include <Arduino.h>
+#include <pins_arduino.h> 
+#undef F
+#define F(string_literal) ((const PROGMEM char *)(string_literal))
+#undef PSTR
+#define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
+>>>>>>> Added Shift register support for LED bars
 
 #define __disable_irq() __asm__ volatile("");
 #define __enable_irq()	__asm__ volatile("");
